6fc10fa0ac2f8bec68f745adc9e22f60d78524ab
Александр Корчагин <akorchagin@astralinux.ru>
2024-11-07 15:58:49 +0300

    lesson3

---
+++ Практические задания по модулю 3/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/dmacvicar/libvirt" {
+  version     = "0.7.1"
+  constraints = "0.7.1"
+  hashes = [
+    "h1:1yEJVPVFkRkbRY63+sFRAWau/eJ0xlecHWLCV8spkWU=",
+  ]
+}
---
+++ Практические задания по модулю 3/main.tf
@@ -0,0 +1,31 @@
+terraform { 
+  required_providers { 
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+  }
+}
+provider "libvirt" {
+  alias = "local"
+  uri = "qemu:///system"
+}
+provider "libvirt" {
+  alias = "remote"
+  uri = "qemu+ssh://ak@10.177.7.195/system?known_hosts=/home/user/.ssh/known_hosts"
+}
+resource "libvirt_network" "terraform_network" {
+  provider = libvirt.remote
+  name = "terrafrom_network"
+  mode = "nat"
+  domain = "terraform.local"
+  addresses = ["10.100.100.0/24"]
+  
+  dns {
+    enabled = true
+  }
+
+  dhcp {
+    enabled = true
+  }
+}
